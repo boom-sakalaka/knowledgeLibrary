2049eb189f0a1008bc0a7c3c5a4802e69b2e8e9c
Gu <936285702@qq.com>
2021-07-11 23:03:47 +0800

    解决spring-boot-maven-plugin插件报红问题，去到maven本地仓库删除对应的包，指定下载的版本，换成阿里云maven镜像，重新下载，

---
+++ doc/all.sql
@@ -0,0 +1,8 @@
+Drop table if exists `test`;
+create table   `test`(
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment '名称',
+    `password` int comment '密码',
+    primary key (`id`)
+) engine=innodb default  charset=utf8mb4 comment='测试';
+
