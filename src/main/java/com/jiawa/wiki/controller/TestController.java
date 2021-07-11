66d76821b0316b27044b9218e04a697f69c56bc7
Gu <936285702@qq.com>
2021-07-11 09:34:56 +0800

    增加自定义参数 @Value 获得 默认值

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,18 +1,21 @@
 package com.jiawa.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${test.hello:TEST}")
+    private String testHell;
     /**
      * GET POST PUT DELETE
      * @return
      */
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World!" + testHell;
     }
 
     @PostMapping("/hello/post")
