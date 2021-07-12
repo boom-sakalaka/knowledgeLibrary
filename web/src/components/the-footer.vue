b5e732f8435dc58d599195eee74b4b3a55394806
Gu <936285702@qq.com>
2021-07-13 07:12:25 +0800

    使用 ref reactive 实现数据绑定

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,26 @@
+<!--
+ * @Author: GZH
+ * @Date: 2021-07-13 07:10:39
+ * @LastEditors: GZH
+ * @LastEditTime: 2021-07-13 07:11:08
+ * @FilePath: \web\src\components\the-footer.vue
+ * @Description: 
+-->
+<template>
+  <a-layout-footer style="text-align: center">
+    电子书
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer',
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
