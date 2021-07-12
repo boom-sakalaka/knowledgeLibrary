8177413eb9a4c7e68a35b3c014b66893291761ef
Gu <936285702@qq.com>
2021-07-13 06:57:57 +0800

    首页样式布局优化

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -2,28 +2,67 @@
  * @Author: GZH
  * @Date: 2021-07-12 21:53:00
  * @LastEditors: GZH
- * @LastEditTime: 2021-07-13 06:20:57
+ * @LastEditTime: 2021-07-13 06:45:52
  * @FilePath: \knowledgeLibrary\web\src\views\Home.vue
  * @Description: 
 -->
 <template>
-  <div class="home">
-    <a-button type="primary">
-      Primary
-    </a-button>
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <a-layout>
+    <a-layout-sider width="200" style="background: #fff">
+      <a-menu
+        mode="inline"
+        v-model:selectedKeys="selectedKeys2"
+        v-model:openKeys="openKeys"
+        :style="{ height: '100%', borderRight: 0 }"
+      >
+        <a-sub-menu key="sub1">
+          <template #title>
+            <span>
+              <user-outlined />
+              subnav 1
+            </span>
+          </template>
+          <a-menu-item key="1">option1</a-menu-item>
+          <a-menu-item key="2">option2</a-menu-item>
+          <a-menu-item key="3">option3</a-menu-item>
+          <a-menu-item key="4">option4</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <template #title>
+            <span>
+              <laptop-outlined />
+              subnav 2
+            </span>
+          </template>
+          <a-menu-item key="5">option5</a-menu-item>
+          <a-menu-item key="6">option6</a-menu-item>
+          <a-menu-item key="7">option7</a-menu-item>
+          <a-menu-item key="8">option8</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub3">
+          <template #title>
+            <span>
+              <notification-outlined />
+              subnav 3
+            </span>
+          </template>
+          <a-menu-item key="9">option9</a-menu-item>
+          <a-menu-item key="10">option10</a-menu-item>
+          <a-menu-item key="11">option11</a-menu-item>
+          <a-menu-item key="12">option12</a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+      Content123
+    </a-layout-content>
+  </a-layout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
 });
 </script>
