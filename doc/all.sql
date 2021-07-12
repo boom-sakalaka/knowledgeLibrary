420cc7b41dbe4cbf5f26c5fe987cb708a4aaa8d2
Gu <936285702@qq.com>
2021-07-12 19:46:47 +0800

    集成 mybatis generator，生成demo表代码

--- doc/all.sql
+++ doc/all.sql
@@ -6,3 +6,13 @@ create table   `test`(
     primary key (`id`)
 ) engine=innodb default  charset=utf8mb4 comment='测试';
 
+
+
+Drop table if exists `demo`;
+create table   `demo`(
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment '名称',
+    primary key (`id`)
+) engine=innodb default  charset=utf8mb4 comment='测试';
+
+insert into `demo` (id,name) values (1,'测试');
