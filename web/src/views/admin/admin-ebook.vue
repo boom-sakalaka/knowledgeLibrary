94c4c15486ba9970a5a06e20f29b420869f2dc79
Gu <936285702@qq.com>
2021-07-14 20:23:21 +0800

    修改电子书页面布局

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -2,12 +2,18 @@
  * @Author: GZH
  * @Date: 2021-07-14 20:11:02
  * @LastEditors: GZH
- * @LastEditTime: 2021-07-14 20:11:49
+ * @LastEditTime: 2021-07-14 20:22:48
  * @FilePath: \web\src\views\admin\admin-ebook.vue
  * @Description: 
 -->
 <template>
-  <div></div>
+  <a-layout>
+    <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+      <div>
+        <h1>电子书管理</h1>
+      </div>
+    </a-layout-content>
+  </a-layout>
 </template>
 
 <script lang="ts">
