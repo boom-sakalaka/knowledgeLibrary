6df59ccf37756e8542e0652ef0d0ebd13065984c
Gu <936285702@qq.com>
2021-07-19 21:19:38 +0800

    增加文档表sql并生成持久层代码

--- doc/all.sql
+++ doc/all.sql
@@ -39,7 +39,7 @@ insert into `ebook` (id,name,description) values (5,'Oracle 入门教程','零�
 
 # 分类表
 
-drop table  if exists  `category`
+drop table  if exists  `category`;
 create table `category`
 (
     `id`     bigint      not null comment 'id',
@@ -64,3 +64,24 @@ insert into `category` (id,parent,name,sort) values (500,000,'其它',500);
 insert into `category` (id,parent,name,sort) values (501,500,'服务器',501);
 insert into `category` (id,parent,name,sort) values (502,500,'开发工具',502);
 insert into `category` (id,parent,name,sort) values (503,500,'热门服务端语言',503);
+
+
+
+drop table if exists `doc`;
+create table `doc`(
+    `id` bigint not null comment 'id',
+    `ebook_id` bigint not null default 0 comment '电子书id',
+    `parent` bigint not null default 0 comment '父id',
+    `name` varchar(50) not null comment '名称',
+    `sort` int comment '顺序',
+    `view_count` int default 0 comment '阅读数',
+    `vote_count` int default 0 comment '点赞数',
+    primary key (`id`)
+)engine=innodb default  charset=utf8mb4 comment='文档';
+
+insert into `doc` (id,ebook_id,parent,name,sort,view_count,vote_count) values (1,1,0,'文档1',1,0,0);
+insert into `doc` (id,ebook_id,parent,name,sort,view_count,vote_count) values (2,1,1,'文档1.1',1,0,0);
+insert into `doc` (id,ebook_id,parent,name,sort,view_count,vote_count) values (3,1,0,'文档2',1,0,0);
+insert into `doc` (id,ebook_id,parent,name,sort,view_count,vote_count) values (4,1,3,'文档2.1',1,0,0);
+insert into `doc` (id,ebook_id,parent,name,sort,view_count,vote_count) values (5,1,3,'文档2.2',2,0,0);
+insert into `doc` (id,ebook_id,parent,name,sort,view_count,vote_count) values (6,1,5,'文档2.2.1',1,0,0);
